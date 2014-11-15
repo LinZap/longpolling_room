9b9fab88c0483e552945e8f36d35870bf3f9e392
Zap <soldierzx0705@gmail.com>
2014-11-15 13:31:35 +0800

    new

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?
 
-    $dbhost = 'zap.comze.com';
+    $dbhost = 'mysql5.000webhost.com';
     $dbuser = 'a1842796_zap';
     $dbpass = 'zxcvbnm123';
     $dbname = 'a1842796_onlyone';
@@ -9,7 +9,6 @@
     
     mysql_query("SET NAMES 'utf8'");
     mysql_select_db($dbname)  or die("無法開啟資料庫$dbname連結!"); 
-
 
 
 ?>
